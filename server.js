ea25e807de4bc6f384c22721b95d224db624ab1b
Teppei Yano <buckelwal@gmail.com>
2012-05-04 20:04:40 +0900

    fix indentation

--- server.js
+++ server.js
@@ -25,31 +25,31 @@ app.configure(function () {
     app.set("view options", {layout: false});
     // make a custom html template
     app.register('.html', {
-	compile: function(str, options){
-	    return function(locals){
-		return str;
-	    };
-	}
+        compile: function(str, options){
+            return function(locals){
+                return str;
+            };
+        }
     });
 });
 
 app.dynamicHelpers({
     session : function (req, res) {
-	return req.session;
+        return req.session;
     }
 });
 
 function createResponseListener (target, eventName) {
     return function (response) {
-	var body = "";
-	response.on('data', function (chunk) {
-	    body += chunk;
-	});
-	response.on('end', function () {
-	    console.log(body);
-	    var ev = JSON.parse(body);
-	    target.emit(eventName, ev);
-	});
+        var body = "";
+        response.on('data', function (chunk) {
+            body += chunk;
+        });
+        response.on('end', function () {
+            console.log(body);
+            var ev = JSON.parse(body);
+            target.emit(eventName, ev);
+        });
     }
 }
 
@@ -77,52 +77,52 @@ app.get('/auth/github/callback', function (req, res) {
     var reqBody = ('client_id='+CLIENT_ID+'&client_secret='+CLIENT_SECRET+'&code='+url.parse(req.url, true).query.code);;
     var request = https.request(
         {
-	    host : GITHUB,
-	    method : 'POST', 
+            host : GITHUB,
+            method : 'POST', 
             path : '/login/oauth/access_token?'+reqBody,
-	    headers : { 'Accept' : 'application/json' }
-	}
+            headers : { 'Accept' : 'application/json' }
+        }
     );
     request.on('response', createResponseListener(request, 'login'));
     request.on('login',  function (e) {
-	if (e.error) res.end(JSON.stringify(e.error));
-	else {
-	    req.session.github = {};
-	    req.session.github.token = e.access_token;
-	    //req.session.redirect_uri = undefined;
-	    req.emit('authDone');
-	}
+        if (e.error) res.end(JSON.stringify(e.error));
+        else {
+            req.session.github = {};
+            req.session.github.token = e.access_token;
+            //req.session.redirect_uri = undefined;
+            req.emit('authDone');
+        }
     });
     request.write('');
     request.end();
 
     req.on('authDone', function () {
-	var request = https.get({
-	    host : GITHUBAPI,
-	    path : '/user?access_token='+req.session.github.token,
-	    headers : {'Accepted' : 'application/json'}
-	});
-	request.on('response', createResponseListener(request, 'parse'));
-	request.on('parse', function (e) {
-	    console.log(e);
-	    req.session.github.id = e.id;
-	    req.session.github.name = e.login;
-	    req.session.github.avatar_url = e.avatar_url;
-	    User.findOne({github_id : e.id}, function (err, doc) {
-		if (!err && doc) {res.redirect(req.session.redirect_uri || '/'); return}
-		if (!err && !doc) {
-		    var user = new User(); //mongoose model User
-		    user.github_id = e.id,
-		    user.name = e.login,
-		    user.avatar_url = e.avatar_url;
-		    user.save(function (err) {
-			if (err) res.end(JSON.stringify(err));
-			else res.redirect(req.session.redirect_uri || '/');
-		    });
-		}
-	    });
-	    setTimeout(function () {res.end('timeout')}, 5000); //this fires when the server is not connected to the  db
-	});
+        var request = https.get({
+            host : GITHUBAPI,
+            path : '/user?access_token='+req.session.github.token,
+            headers : {'Accepted' : 'application/json'}
+        });
+        request.on('response', createResponseListener(request, 'parse'));
+        request.on('parse', function (e) {
+            console.log(e);
+            req.session.github.id = e.id;
+            req.session.github.name = e.login;
+            req.session.github.avatar_url = e.avatar_url;
+            User.findOne({github_id : e.id}, function (err, doc) {
+                if (!err && doc) {res.redirect(req.session.redirect_uri || '/'); return}
+                if (!err && !doc) {
+                    var user = new User(); //mongoose model User
+                    user.github_id = e.id,
+                    user.name = e.login,
+                    user.avatar_url = e.avatar_url;
+                    user.save(function (err) {
+                        if (err) res.end(JSON.stringify(err));
+                        else res.redirect(req.session.redirect_uri || '/');
+                    });
+                }
+            });
+            setTimeout(function () {res.end('timeout')}, 5000); //this fires when the server is not connected to the  db
+        });
     });
 });
 
@@ -134,17 +134,17 @@ app.get('/auth/github/callback', function (req, res) {
 app.get('/user/:name', function (req, res) {
     res = setCommonHeader(res);
     User.findOne({name : req.params.name, }, function (err, doc) {
-	if (!err && doc) {
-	    var doc = JSON.stringify(doc);
-	    res.setHeader('Content-Length', Buffer.byteLength(doc, 'utf8'));
-	    res.end(doc);
-	    return;
-	} else {
+        if (!err && doc) {
+            var doc = JSON.stringify(doc);
+            res.setHeader('Content-Length', Buffer.byteLength(doc, 'utf8'));
+            res.end(doc);
+            return;
+        } else {
             var err = JSON.stringify({"error" : "user "+req.params.name+" does not exist"});
-	    res.setHeader('Content-Length', Buffer.byteLength(err, 'utf8'));
-	    res.end(err);
-	    return;
-	}
+            res.setHeader('Content-Length', Buffer.byteLength(err, 'utf8'));
+            res.end(err);
+            return;
+        }
     });
 });
 
@@ -157,40 +157,40 @@ app.put('/user', function (req, res) {
     res = setCommonHeader(res);
 
     function validate (v) {
-	return v.length > 0 && v.length < 2000 && !(v.match(/[|]|{|}|<|>|&|;|"|`|=/));
+        return v.length > 0 && v.length < 2000 && !(v.match(/[|]|{|}|<|>|&|;|"|`|=/));
     }
     var change = {};
     _(req.body.changeSet).each(function (item,key) {
-	if (key === 'name' || key === 'github_id' || key === '_id') return;
-	else if (!validate(item)) return;
-	else change[key] = item;
+        if (key === 'name' || key === 'github_id' || key === '_id') return;
+        else if (!validate(item)) return;
+        else change[key] = item;
     });
     change.updatedAt = new Date().getTime();
     console.log(change);
     var conditions = {github_id : req.session.github.id},
-    update = {$set : change},
-    options = {};
+        update = {$set : change},
+        options = {};
     console.log(update);
     User.update(conditions, update, options, function (err) {
-	if (err) {
-	    var err = JSON.stringify(err);
-	    res.setHeader('Content-Length', Buffer.byteLength(err, 'utf8'));
-	    res.end(err);
-	}
-	else {
-	    res.setHeader('Content-Length', 13);
-	    res.end('{success : 1}');
-	}
+        if (err) {
+            var err = JSON.stringify(err);
+            res.setHeader('Content-Length', Buffer.byteLength(err, 'utf8'));
+            res.end(err);
+        }
+        else {
+            res.setHeader('Content-Length', 13);
+            res.end('{success : 1}');
+        }
     });
 });
 
 app.get('/me', function (req, res) {
     if (!req.session || !req.session.github || !req.session.github.id) {
-	res.redirect('/auth/github?redirect_uri=/me');
-	return;
+        res.redirect('/auth/github?redirect_uri=/me');
+        return;
     }
     User.findOne({github_id : req.session.github.id}, function (err, doc) {
-	if (!err && doc) res.render('update.html');
+        if (!err && doc) res.render('update.html');
     });
 });
 
@@ -202,15 +202,15 @@ app.get('/whoami', function (req, res) {
     res = setCommonHeader(res);
 
     if (!!req.session && !!req.session.github) {
-      var json = JSON.stringify({
-	name : req.session.github.name
-      });
-      res.setHeader('Content-Length', Buffer.byteLength(json, 'utf8'));
-      res.end(json);
+        var json = JSON.stringify({
+            name : req.session.github.name
+        });
+        res.setHeader('Content-Length', Buffer.byteLength(json, 'utf8'));
+        res.end(json);
     }
     else {
-      res.setHeader('Content-Length', 17);
-      res.end('{name : "noone""}');
+        res.setHeader('Content-Length', 17);
+        res.end('{name : "noone""}');
     }
 });
 
@@ -222,9 +222,9 @@ app.get('/dl', function (req, res) {
 
 app.get('/recent', function (req, res) {
     User.find({}).limit(20).sort("updatedAt", -1).execFind(function (err, docs) {
-	var json = JSON.stringify((docs || err))
+        var json = JSON.stringify((docs || err))
         res.setHeader('Content-Length', Buffer.byteLength(json, 'utf8'));
-	res.end(json);
+        res.end(json);
     })
 });
 
@@ -233,3 +233,4 @@ app.get('/:userName', function (req, res) {
 });
 
 app.listen(8080);
+
